5e26044b83c5a91faefc0a02efecf015883f15fd
Mikhail Dedeiko <mikhail.dedeiko@gmail.com>
2023-03-03 09:59:04 +0100

    Updated client

--- SSL/client-server/client.js
+++ SSL/client-server/client.js
@@ -1,39 +1,41 @@
-const https = require('https')
-const fs = require('fs')
+const https = require("https");
+const fs = require("fs");
 
-const clientCertKey = fs.readFileSync('../localhost-with-ca/localhost_client.key');
-const clientCert = fs.readFileSync('../localhost-with-ca/localhost_client.crt');
-const serverCA = fs.readFileSync('../localhost-with-ca/localhost-ca_client.crt');
+const clientCertKey = fs.readFileSync(
+  "../localhost-with-ca/localhost_client.key"
+);
+const clientCert = fs.readFileSync("../localhost-with-ca/localhost_client.crt");
+const serverCA = fs.readFileSync("../localhost-with-ca/localhost-ca.crt");
 
 const options = {
-  hostname: 'localhost',
+  hostname: "localhost",
   rejectUnauthorized: true,
   port: 8000,
-  path: '/',
-  method: 'GET',
+  path: "/",
+  method: "GET",
   key: clientCertKey,
   cert: clientCert,
-  ca: serverCA
-}
+  ca: serverCA,
+};
 
 //////////// base request
 
-const req = https.request(options, res => {
-  const cert = res.req.socket.getPeerCertificate(true)
-  console.log(cert.raw.toString('base64'));
+const req = https.request(options, (res) => {
+  const cert = res.req.socket.getPeerCertificate(true);
+  console.log(cert.raw.toString("base64"));
   console.log(cert);
-  console.log(JSON.stringify(cert.issuer))
-  console.log(JSON.stringify(cert.subject))
+  console.log(JSON.stringify(cert.issuer));
+  console.log(JSON.stringify(cert.subject));
 
-  console.log(`statusCode: ${res.statusCode}`)
+  console.log(`statusCode: ${res.statusCode}`);
 
-  res.on('data', d => {
-    process.stdout.write(d)
-  })
-})
+  res.on("data", (d) => {
+    process.stdout.write(d);
+  });
+});
 
-req.on('error', error => {
-  console.error(error)
-})
+req.on("error", (error) => {
+  console.error(error);
+});
 
-req.end()
+req.end();
